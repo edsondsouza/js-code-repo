a0a9b19da8334376a8bc96fafc532b2e194f7e4b
Edson Sooraj Dsouza <edsondsouzadev@gmail.com>
2023-10-29 16:07:33 +0000

    added variables file

---
+++ basics/variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553;
+let accountEmail = 'edson@google.com';
+var accontPasswd = '1221212';
+accountCity = 'Bengaluru';
+let accountState; //undefined
+
+console.table([accountId, accountEmail, accontPasswd, accountCity, accountState]);
+
+accountEmail = 'dsouza@d.com';
+accontPasswd = '41414141';
+accountCity = 'Jaipur'
+
+console.table([accountId, accountEmail, accontPasswd, accountCity, accountState]);
+
+// this is a single line comment
+
+/* this is a 
+multi-line comment
+*/
+
