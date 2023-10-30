4168170a66ba23537cddbe4d7b5f851fddcb0470
Edson Sooraj Dsouza <edsondsouzadev@gmail.com>
2023-10-30 15:16:40 +0000

    comparisons file added, learnt about comparisons

---
+++ basics/comparisons.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+/* prevent such type of conversion. **Remember**: Only clean code always has the value
+// console.log(null > 0) // false
+// console.log(null == 0) // false
+// console.log(null >= 0) // true
+
+// console.log(undefined > 0) // false
+// console.log(undefined == 0) // false
+// console.log(undefined >= 0) // false
+*/
+
+// strict comparison ===
