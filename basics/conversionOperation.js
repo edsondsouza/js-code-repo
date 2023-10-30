a5f91c32cd83e6d58aad69e3824cefcb0c906281
Edson Sooraj Dsouza <edsondsouzadev@gmail.com>
2023-10-30 13:28:10 +0000

    conversion operation file added

---
+++ basics/conversionOperation.js
@@ -0,0 +1,31 @@
+let score = true // 33, "33", "33abd", null, undefined, true, false
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/* 
+33 => 33
+"33" => 33
+"33abc" => NaN
+null => 0
+undefined => NaN
+true = 1; false = 0
+*/
+
+let isLoggedIn = "Edson" // 1, "", "Edson"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33 // 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// 1 => true
+// "" => false
+// "Edson" => true
+
+// 33 => conversion from number to string, the output will look like number but when you "typeof" the output will be displayed as string.
