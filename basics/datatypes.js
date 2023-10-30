900f3e573966be106f40c18d7be6c7cc02577aee
Edson Sooraj Dsouza <edsondsouzadev@gmail.com>
2023-10-30 12:59:03 +0000

    datatypes file added

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+// code redability should be high
+
+// official documentation of js: https://tc39.es/ecma262/
+
+let name = "Edson"
+let age = 21
+let isLoggedIn = true // false
+
+/* Primitive Datatypes */
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// objects
+
+console.log(typeof undefined); // o/p: undefined - undefined in javascript returns undefined. This is because undefined is a type in javascript. But incase of null, it is a standalone value.
+console.log(typeof null); // o/p: object - null in javascript is an object. Few developers call it as the error in javascript. This can be asked in interview **Remember**.
