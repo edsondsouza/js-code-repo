08587aaf2872c02b3d484f56d3ce9b99ffe21a1f
edson <edsondsouzadev@gmail.com>
2023-11-05 11:06:15 +0530

    practice variables

---
+++ practice/variables.js
@@ -0,0 +1,14 @@
+const userId = 12
+let username = "edson"
+var userPassword = "12345"
+
+// console.log(userId)
+// console.log(username)
+// console.log(userPassword)
+
+// console.log(userId, username, userPassword)
+
+console.table([userId, username, userPassword])
+
+// single line comment
+/* multi-line comment */
