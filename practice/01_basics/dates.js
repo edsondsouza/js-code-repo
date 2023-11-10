070d6461a06653f16852bf0be1d560f1cbd22935
Edson Sooraj Dsouza <edsondsouzadev@gmail.com>
2023-11-10 00:10:22 +0000

    dates in js revision

---
+++ practice/01_basics/dates.js
@@ -0,0 +1,28 @@
+const date = Date()
+// console.log(date)
+
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+
+// let someDate = new Date(2023, 0, 2, 5, 30)
+// let someDate = new Date("2023-2-14")
+let someDate = new Date("2-14-2023")
+
+// console.log(someDate.toString())
+// console.log(someDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(myTimeStamp.getTime)
+// console.log(Math.floor(Date.now()))
+
+let newDate = new Date();
+console.log(newDate)
+console.log(newDate.toString())
+console.log(newDate.getMonth() + 1)
+console.log(newDate.getDay())
