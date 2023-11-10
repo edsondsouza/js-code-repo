276d8f8394791e5e2413ae5d145e4466bc316b6f
Edson Sooraj Dsouza <edsondsouzadev@gmail.com>
2023-11-10 14:40:22 +0000

    while and do-while discussion

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`)
+//     index = index + 2; // index += 2
+// }
+
+let myarr = ['superman', 'spiderman', 'batman']
+let arrindex = 0
+// while (arrindex < myarr.length) {
+//     // console.log(`Value is: ${arrindex}`) // => prints the index
+//     console.log(`Value is: ${myarr[arrindex]}`) // => prints the value
+//     arrindex = arrindex + 1
+// }
+
+// do-while
+
+let score = 11
+do {
+    console.log(`Score is: ${score}`)
+    score++
+} while (score <= 10);
+
+// do-while mei pehle kaam hota hei aur baad mei condition check hota hei
