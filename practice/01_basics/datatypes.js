b4e3890e53f9fd58b6f8717060c9585daf7293a1
Edson Sooraj Dsouza <edsondsouzadev@gmail.com>
2023-11-13 05:34:53 +0000

    datatypes practice

---
+++ practice/01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // newer version of js mei treat karlo
+// let, const, var
+
+// alert("bhopender jogi")
+
+let name = "edson" // string
+let age = 21 // number
+let isLoggedIn = false // boolean
+
+// Primitive types
+/* 
+Number, String, Boolean, BigInt, Symbol, Undefined => type undefined, 
+Null => standalone value
+*/
+
+// Reference or non-primitive types (),{},[]
+// objects
+
+console.log(typeof null) // => null is an object in javascript. Few developers call this as an error of javascrpt
+console.log(typeof undefined) // => undefined is a proper type in javascript
