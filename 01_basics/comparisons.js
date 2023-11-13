156b6a85862eee4cda2e7585d1e60fa2a6d38c1e
Edson Sooraj Dsouza <edsondsouzadev@gmail.com>
2023-11-13 07:08:28 +0000

    comparision discussion

--- 01_basics/comparisons.js
+++ 01_basics/comparisons.js
@@ -8,14 +8,14 @@
 console.log("2" > 1);
 console.log("02" > 1);
 
-/* prevent such type of conversion. **Remember**: Only clean code always has the value
+/* prevent such type of conversion. **Remember**: Only clean code always has the value */
 // console.log(null > 0) // false
 // console.log(null == 0) // false
-// console.log(null >= 0) // true
+// console.log(null >= 0) // true => remember this one just in case of interview point
 
 // console.log(undefined > 0) // false
 // console.log(undefined == 0) // false
 // console.log(undefined >= 0) // false
-*/
+
 
 // strict comparison ===
---
+++ practice/01_basics/comparision.js
@@ -0,0 +1,17 @@
+// normal comparisions: <, >, <=, >=, ==, !=, 
+
+// strict comparisions: ===, !==
+
+// unnecessary comparisions mat karo. jaise ki undefined ko numbers k sath, null ko undefined k sath, string ko number k sath.
+
+// console.log("2" >= 1);
+// console.log("02" > 1);
+
+/* prevent such type of conversion. **Remember**: Only clean code always has the value */
+// console.log(null > 0) // false
+// console.log(null == 0) // false
+// console.log(null >= 0) // true => remember this one just in case of interview point
+
+console.log(undefined > 0) // false
+console.log(undefined == 0) // false
+console.log(undefined >= 0) // false
